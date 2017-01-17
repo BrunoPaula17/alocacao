9c824d2b7d2b82387443600813345182f98df025
josevieiradasilvafilho <jose.da.silva.filho@avanade.com>
2017-01-17 16:01:21 -0200

    interface

---
+++ app/professional/professional.interface.ts
@@ -0,0 +1,10 @@
+export interface professionalInterface {
+    
+    PID: number;
+    EID: string;
+    Name: string;
+    Email: string;
+    Office:string;
+    Prefix: number;
+    Phone: number
+}
