ddca35168952e5255aaf57768fb955d437d5fe7e
Bruno Cataldo <b.cataldo.da.silva@avanade.com>
2017-01-17 15:30:09 -0200

    Criacao arquivos ts - Project

---
+++ app/project/project.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { ProjectComponent } from './project.component'
+
+@NgModule({
+    imports: [ BrowserModule ],
+    declarations: [ ProjectComponent ],
+    bootstrap: [ ProjectComponent ]
+})
+
+export class ProjectModule { }
