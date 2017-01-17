74ce70d756d0231e75d34dd2ef9eb80c8be7c2d5
Paloma CP <p.cristina.pereira@avanade.com>
2017-01-17 17:25:18 -0200

    Role page added to project. Paloma e Thais.

---
+++ app/role/role.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core'
+import { Role } from './role'
+
+@Component({
+    selector: 'ava-rol-app',
+    templateUrl: './app/role/role.html'
+})
+export class RoleComponent {
+    model: Role = {
+        "pageName":"Cargos",
+        "roleId": 5,
+        "name": "Senior System Analyst",
+        "brc": "2017SSA",
+        "level": 8,
+        "description":"Senior software development"
+    };
+}
