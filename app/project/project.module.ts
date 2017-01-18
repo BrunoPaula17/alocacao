27ae8fb87cd6fc93779c482d4df916e03d1f5af5
Bruno Cataldo <b.cataldo.da.silva@avanade.com>
2017-01-18 16:30:32 -0200

    Listsa de projetos

--- app/project/project.module.ts
+++ app/project/project.module.ts
@@ -4,10 +4,11 @@ import { FormsModule }   from '@angular/forms';
 
 import { ProjectComponent } from './project.component'
 import { ProjectFormComponent } from './project.form.component'
+import { ProjectListComponent } from './project.list.component'
 
 @NgModule({
     imports: [ BrowserModule, FormsModule ],
-    declarations: [ ProjectComponent, ProjectFormComponent ],
+    declarations: [ ProjectComponent, ProjectFormComponent, ProjectListComponent ],
     bootstrap: [ ProjectComponent ]
 })
 
