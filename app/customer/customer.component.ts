bfcad89c3cf3bcfd43229191fb3bd3e75dc78d8e
Diego SS Matos <diego.ss.matos@avanade.com>
2017-01-18 15:37:19 -0200

    Customer

--- app/customer/customer.component.ts
+++ app/customer/customer.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core'
-import { Customer } from './customer'
+import { Component } from '@angular/core';
+import { Customer } from './customer';
+import { CUSTOMERS } from '../shared/mock';
 
 @Component({
     selector: 'ava-cust-app',
     templateUrl: './app/customer/customer.html'
 })
 export class CustomerComponent {
+    customers: Customer[] = CUSTOMERS;
     pageName: string = "Cliente";
+    currentCustomer : Customer; 
+
+   OnLineClick(customer:Customer) : void
+   {
+       this.currentCustomer = customer;
+   }
 
    
-}
+}   
