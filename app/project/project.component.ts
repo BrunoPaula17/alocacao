abc2879c92a0d12de7ae72d12f2ed992a1341d0a
Bruno de Paula <b.schneider.de.paula@avanade.com>
2017-01-18 14:23:39 -0200

    Arrumando mock.ts

--- app/project/project.component.ts
+++ app/project/project.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core'
-import { Project } from './project'
+import { Component } from '@angular/core';
+import { Project } from './project';
 
 @Component({
     selector: 'ava-prj-app',
