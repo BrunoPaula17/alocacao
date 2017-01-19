cc2ea60e3aebfdc360ed3016ec72b19405a9c844
Bruno Cataldo <b.cataldo.da.silva@avanade.com>
2017-01-19 17:08:13 -0200

    Correção do grid-header

--- app/shared/grid/grid-header.ts
+++ app/shared/grid/grid-header.ts
@@ -3,9 +3,4 @@ export class Header{
     description: string;
     class: string;
     filter: string;
-    Edit(obj: Object){}
-    Delete(obj: Object){}
-    Update(obj: Object){}
-    Insert(obj: Object){}
-    Custom(obj: Object){}
 }
