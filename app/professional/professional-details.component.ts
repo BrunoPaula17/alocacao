937e9739d1c4be25880666af0318b5cd2797dd0b
jose.da.silva.filho <jose.da.silva.filho@avanade.com>
2017-01-19 16:31:20 -0200

    Update Professional separaçao Header

---
+++ app/professional/professional-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { Professional } from './professional';
+import { Role } from '../role/role';
+
+
+@Component({
+    moduleId: module.id,
+    selector:'ava-pro-dtl-app',
+    templateUrl:'professional-details.html'
+})
+export class ProfessionalDetail{
+    @Input() professionals: Professional;
+    @Input() role: Role[];
+   
+}
--- app/professional/professional.module.ts
+++ app/professional/professional.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { PhoneFilter } from '../shared/phone.filter'
 
 import { ProfessionalComponent } from './professional.component'
+import { ProfessionalDetail } from './professional-details.component';
+import { ProfessionalService } from './Professional.service';
+
+import { HeaderComponent } from '../shared/header/header.component';
 
 @NgModule({
     imports: [ BrowserModule ],
-    declarations: [ ProfessionalComponent, PhoneFilter ],
+    declarations: [ ProfessionalComponent, 
+                    ProfessionalDetail, 
+                    PhoneFilter,
+                    HeaderComponent ],
+    providers: [ ProfessionalService ],
     bootstrap: [ ProfessionalComponent ]
 })
 
