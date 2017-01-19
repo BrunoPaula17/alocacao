9227df66c52985192362807966931a62d9496439
Bruno de Paula <b.schneider.de.paula@avanade.com>
2017-01-19 17:37:37 -0200

    Configuração de Rotas

--- app/booking/booking.module.ts
+++ app/booking/booking.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { PercentualFilter } from '../shared/percentual.filter'
+import { RouterModule, Router } from '@angular/router';
 
 import { BookingComponent } from './booking.component';
 import { BookingDetailComponent } from './booking-details.component';
@@ -9,13 +10,16 @@ import { BookingService } from './booking.service';
 import { HeaderComponent } from '../shared/header/header.components';
 
 @NgModule({
-    imports: [ BrowserModule ],
-    declarations: [ BookingComponent,
-                    BookingDetailComponent,
-                    PercentualFilter, 
-                    HeaderComponent ],
-    providers: [ BookingService ],
-    bootstrap: [ BookingComponent ]
+    imports: [BrowserModule,
+        RouterModule.forChild([
+            { path: 'bookings', component: BookingComponent }
+        ])],
+    declarations: [BookingComponent,
+        BookingDetailComponent,
+        PercentualFilter,
+        HeaderComponent],
+    providers: [BookingService],
+    bootstrap: [BookingComponent]
 })
 
 export class BookingModule { }
