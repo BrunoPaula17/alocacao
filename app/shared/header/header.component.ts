0df43a0f36e16c3eedf3200644f2800af5d2f684
Bruno de Paula <b.schneider.de.paula@avanade.com>
2017-01-20 15:29:57 -0200

    Adicionando componente

--- app/shared/header/header.component.ts
+++ app/shared/header/header.component.ts
@@ -1,10 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
     moduleId: module.id,
     selector: 'ava-hdr-app',
     templateUrl: 'header.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+    
     @Input() pageName: string;
+    @Input() icon: string;
+             iconClass: string;
+
+    ngOnInit(): void {
+        this.iconClass = `fa ${this.icon}`;
+    }
 }
