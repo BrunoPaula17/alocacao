f66b03ab5eb0db1029d5fba4a025bd60faa749fd
Samara Cipriano <samara.martins@avanade.com>
2017-01-20 12:51:50 -0200

    Inclusão do botão voltar na tela de detalhes

--- app/professional/professional-detail.component.ts
+++ app/professional/professional-detail.component.ts
@@ -1,7 +1,10 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Professional } from "./professional";
 import { ProfessionalService} from "./professional.service";
+
 import { RoleService} from "../role/role.service";
 import { Role } from "../role/role"
 
@@ -16,7 +19,8 @@ export class ProfessionalDetailComponent implements OnInit {
 
     constructor(private _professionalService:ProfessionalService,
                 private _roleService:RoleService,
-                private _router:ActivatedRoute){}
+                private _router:ActivatedRoute,
+                private _location: Location){}
 
     professional: Professional;
     @Input() role: Role[];
@@ -24,6 +28,10 @@ export class ProfessionalDetailComponent implements OnInit {
     getDetail(id:number): void {
         this._professionalService.getProfessional(id)
             .then(professional => this.professional = professional)
+    }
+
+    goBack():void {
+        this._location.back();
     }
 
     ngOnInit(): void{
