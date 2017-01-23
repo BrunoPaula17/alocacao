3dc0757684c61f192557b511362ecdf72ee6ff06
Diego SS Matos <diego.ss.matos@avanade.com>
2017-01-23 10:04:25 -0200

    Modificacao estranha que eu nao fiz

---
+++ api/persistence/professional.persistence.js
@@ -0,0 +1,56 @@
+"use strict";
+var ProfessionalPersistence = (function () {
+    function ProfessionalPersistence() {
+        this.Professional = [
+            {
+                "pid": 10715376,
+                "eid": "ronye.peterson.anjos",
+                "name": "Ronye Peterson Martins dos Anjos",
+                "email": "ronye.peterson.anjos@avanade.com",
+                "roleID": 1,
+                "prefix": 11,
+                "phone": "971537512",
+                "role": null
+            },
+            {
+                "pid": 10715377,
+                "eid": "thomas.anjos",
+                "name": "Thomas Jefferson Martins dos Anjos",
+                "email": "thomas.anjos@avanade.com",
+                "roleID": 2,
+                "prefix": 11,
+                "phone": "963484588",
+                "role": null
+            },
+            {
+                "pid": 10715378,
+                "eid": "elziria.anjos",
+                "name": "Elziria Martins dos Anjos",
+                "email": "elziria.dos.anjos@avanade.com",
+                "roleID": 3,
+                "prefix": 11,
+                "phone": "984841212",
+                "role": null
+            },
+            {
+                "pid": 10715379,
+                "eid": "jose.dos.anjos",
+                "name": "Jose dos Anjos",
+                "email": "jose.dos.anjos@avanade.com",
+                "roleID": 4,
+                "prefix": 11,
+                "phone": "931312323",
+                "role": null
+            }
+        ];
+    }
+    ProfessionalPersistence.prototype.getProfessinals = function () {
+        return this.professional;
+    };
+    ProfessionalPersistence.prototype.getProfessionals = function (id) {
+        return this.professional.find(function (professional) { return professional.professionalID === id; });
+    };
+    return ProfessionalPersistence;
+}());
+exports.ProfessionalPersistence = ProfessionalPersistence;
+//# sourceMappingURL=professional.persistence.js.map
