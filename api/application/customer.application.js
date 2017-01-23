e2f3d64ad1e8778409d92dd9526a2eeb5f468c6a
Diego SS Matos <diego.ss.matos@avanade.com>
2017-01-23 14:35:57 -0200

    Customer: API

--- api/application/customer.application.js
+++ api/application/customer.application.js
@@ -1,15 +1,26 @@
 "use strict";
 var customer_persistence_1 = require("../persistence/customer.persistence");
+var professional_application_1 = require("./professional.application");
 var CustomerApplication = (function () {
     function CustomerApplication() {
     }
     CustomerApplication.prototype.getCustomers = function () {
         var customerPersistence = new customer_persistence_1.CustomerPersistence();
-        return customerPersistence.getCustomers();
+        var professionalApp = new professional_application_1.ProfessionalApplication();
+        var costumers;
+        var professionals;
+        costumers = customerPersistence.getCustomers();
+        professionals = professionalApp.getProfessionals();
+        costumers.forEach(function (customer) {
+            customer.professional = professionals.find(function (professional) { return professional.pid == customer.responsible; });
+        });
+        return costumers;
     };
     CustomerApplication.prototype.getCustomer = function (id) {
         var customerPersistence = new customer_persistence_1.CustomerPersistence();
-        return customerPersistence.getCustomer(id);
+        var costumer;
+        costumer = customerPersistence.getCustomer(id);
+        return costumer;
     };
     return CustomerApplication;
 }());
--- api/application/customer.application.ts
+++ api/application/customer.application.ts
@@ -1,16 +1,31 @@
 import { Customer } from '../../app/customer/customer';
+import { Professional } from '../../app/professional/professional';
 import { CustomerPersistence } from '../persistence/customer.persistence';
+import { ProfessionalApplication } from './professional.application';
 
 export class CustomerApplication{
+
     getCustomers(): Customer[]{
         let customerPersistence: CustomerPersistence = new CustomerPersistence();
+        let professionalApp: ProfessionalApplication = new ProfessionalApplication();
+        let costumers: Customer[];
+        let professionals: Professional[];
 
-        return customerPersistence.getCustomers();
+        costumers       = customerPersistence.getCustomers();
+        professionals   = professionalApp.getProfessionals();
+
+        costumers.forEach(customer =>{
+            customer.professional = professionals.find(professional => professional.pid == customer.responsible);
+        })
+
+        return costumers;
     }
 
     getCustomer(id: number): Customer{
         let customerPersistence: CustomerPersistence = new CustomerPersistence();
+        let costumer: Customer;
 
-        return  customerPersistence.getCustomer(id);
+        costumer = customerPersistence.getCustomer(id);
+        return costumer;
     }
 }
