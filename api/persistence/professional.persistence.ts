c87588506f6d779309776ee234403fe14fb03014
josevieiras <jose.da.silva.filho@avanade.com>
2017-01-26 15:53:24 -0200

    Update Persistance Delete

--- api/persistence/professional.persistence.ts
+++ api/persistence/professional.persistence.ts
@@ -1,22 +1,9 @@
 import {Professional } from '../../app/professional/Professional';
+import { MongoClient, Db, UpdateWriteOpResult } from 'mongodb';
 import { ICrud } from './crud.interface';
-import { MongoClient, Db } from 'mongodb';
 import { mongoUrl } from '../server';
 
 export class ProfessionalPersistence implements ICrud<Professional>{
-
-    create(professional: Professional): Promise<Professional> {
-        let database: Db = null;
-        return Promise.resolve(MongoClient.connect(mongoUrl)
-            .then((db: Db) => {
-                database = db;
-                return db.collection('professionals').insert(JSON.stringify(professional));
-            })
-            .then(() => {
-                database.close();
-                 console.log("Inserted a document into the customers collection");                
-            }));
-    }
 
     list(): Promise<Professional[]> {
 
@@ -33,17 +20,56 @@ export class ProfessionalPersistence implements ICrud<Professional>{
         }));
     }
 
-    read(id: number): Promise<Professional> {
-        return null;
+    create(professional: Professional): Promise<Professional> {
+
+        let database: Db = null;
+
+        return Promise.resolve(MongoClient.connect(mongoUrl)
+            .then((db: Db) => {
+                database = db;
+                return db.collection('professionals').insert(JSON.stringify(professional));
+            })
+            .then(() => {
+                database.close();
+                 console.log("Inserted a document into the customers collection");                
+            }));
+    }
+   
+
+    read(pid: number): Promise<Professional> {
+
+        let database: Db = null;
+
+        return Promise.resolve(MongoClient.connect(mongoUrl)
+            .then((db: Db) => {
+                database=db;
+                return db.collection ('professionals').findOne({ "deleted": false, "professionalPID": pid });
+            })
+            .then((professional : Professional) => {
+                database.close();
+                return professional;
+            })
+        );
     }
 
-    update(custUpd: Professional): Promise<Professional> {
+    update(professionalUpdate: Professional): Promise<Professional> {
+        
+        let database: Db;
 
-        return null;
+        return Promise.resolve(
+            MongoClient.connect(mongoUrl)
+                .then((db: Db) => {
+                    let _professionalUpdate: Professional ;
+                    return db.collection('professionals').findOneAndUpdate({ bookingID: professionalUpdate.pid }, professionalUpdate);
+                })
+                .then((updateResult: UpdateWriteOpResult) => {
+                    if (updateResult.result.ok == 1)
+                        return professionalUpdate;
+                }));
     }
 
     delete(id: number): Promise<boolean> {
-
-        return null;
+        return Promise.resolve(false);
+       
     }
 }
