7181a20c6c78dff54cdbb4d6d29fb63b43f97ecd
Diego SS Matos <diego.ss.matos@avanade.com>
2017-01-26 16:59:34 -0200

    mock comment =)

--- app/shared/mock.ts
+++ app/shared/mock.ts
@@ -4,41 +4,41 @@ import { Customer } from '../customer/customer';
 import { Role } from '../role/role';
 import { Project } from '../project/project';
 
-export const BOOKINGS: Booking[] = [
-    {
-        "bookingID": 1,
-        "projectID": 1,
-        "pid": 10715376,
-        "auditID": 1,
-        "initialDate": new Date("2017-02-17T02:00:00.000Z"),
-        "endDate": new Date("2017-03-03T03:00:00.000Z"),
-        "bookingPercentual": 1,
-        "project": null,
-        "professional": null
-    },
-    {
-        "bookingID": 1,
-        "projectID": 1,
-        "pid": 10715377,
-        "auditID": 1,
-        "initialDate": new Date("2017-02-16T02:00:00.000Z"),
-        "endDate": new Date("2017-03-03T03:00:00.000Z"),
-        "bookingPercentual": 1,
-                "project": null,
-        "professional": null
-    },
-    {
-        "bookingID": 1,
-        "projectID": 1,
-        "pid": 10715378,
-        "auditID": 1,
-        "initialDate": new Date("2017-02-16T02:00:00.000Z"),
-        "endDate": new Date("2017-03-03T03:00:00.000Z"),
-        "bookingPercentual": 1,
-                "project": null,
-        "professional": null
-    }
-];
+// export const BOOKINGS: Booking[] = [
+//     {
+//         "bookingID": 1,
+//         "projectID": 1,
+//         "pid": 10715376,
+//         "auditID": 1,
+//         "initialDate": new Date("2017-02-17T02:00:00.000Z"),
+//         "endDate": new Date("2017-03-03T03:00:00.000Z"),
+//         "bookingPercentual": 1,
+//         "project": null,
+//         "professional": null
+//     },
+//     {
+//         "bookingID": 1,
+//         "projectID": 1,
+//         "pid": 10715377,
+//         "auditID": 1,
+//         "initialDate": new Date("2017-02-16T02:00:00.000Z"),
+//         "endDate": new Date("2017-03-03T03:00:00.000Z"),
+//         "bookingPercentual": 1,
+//                 "project": null,
+//         "professional": null
+//     },
+//     {
+//         "bookingID": 1,
+//         "projectID": 1,
+//         "pid": 10715378,
+//         "auditID": 1,
+//         "initialDate": new Date("2017-02-16T02:00:00.000Z"),
+//         "endDate": new Date("2017-03-03T03:00:00.000Z"),
+//         "bookingPercentual": 1,
+//                 "project": null,
+//         "professional": null
+//     }
+// ];
 
 export const CUSTOMERS: Customer[] = [    {
         "customerID": 1,
